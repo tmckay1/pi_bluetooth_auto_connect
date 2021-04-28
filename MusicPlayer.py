a1789103e2067efccbbc51e9555504c4e6b2a906
Angeline Canullas <acanullas@Angelines-MacBook-Pro.local>
2021-04-28 19:56:42 -0400

    Add music player

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -1,3 +1,5 @@
+import pygame
+
 """Class to encapsulate a player object to play music"""
 class MusicPlayer:
 
@@ -8,4 +10,11 @@ class MusicPlayer:
     self.device_mac_addresses_to_mp3 = device_mac_addresses_to_mp3
 
   def play(self, mac_address):
-    print("Playing music for: " + self.device_mac_addresses_to_mp3[mac_address])
+    audio_file_path = self.device_mac_addresses_to_mp3[mac_address]
+    print("Playing music for: " + audio_file_path)
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio_file_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+
