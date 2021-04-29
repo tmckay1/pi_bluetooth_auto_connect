fb63e4c253a09258064ecdd4798fdf8bd77cd36c
Tyler McKay <tmckay1.jackson@gmail.com>
2021-04-28 21:15:21 -0400

    add gig address

--- testAutoPair.py
+++ testAutoPair.py
@@ -5,8 +5,8 @@ import MusicPlayer
 
 # A dict of MAC Addresses pointing to an MP3 to play when we connect to the address
 device_mac_addresses_to_mp3 = {
-  "C4:98:80:E0:8F:01": "ty.mp3",
-  "90:B0:ED:DF:AD:CD": "gig.mp3"
+  "C4:98:80:E0:8F:01": "/home/pi/ty.mp3",
+  "90:B0:ED:DF:AD:CD": "/home/pi/gig.mp3"
 }
 
 # The list of MAC Addresses to try to connect to
